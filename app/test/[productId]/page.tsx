c619cd2bac98d0d2d4664f1c57e2a4eabafb421d
mp-104 <mp-104@hotmail.com>
2024-11-05 11:53:42 +0100

    did stuff during class

---
+++ app/test/[productId]/page.tsx
@@ -0,0 +1,10 @@
+export default function productId (
+
+) {
+    return (
+        <main>
+            <h1>product</h1>
+
+        </main>
+    )
+}
---
+++ app/test/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Test() {
+    return (
+        <main>
+            <h1>This is the Test Page</h1>
+            <Link href={"/"} >To Home</Link>
+        </main>
+    )
+}
