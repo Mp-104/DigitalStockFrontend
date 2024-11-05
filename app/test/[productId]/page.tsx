5cfde1b0391fcd21cc495b28c6bd8dc5ed4ab401
mp-104 <mp-104@hotmail.com>
2024-11-05 15:58:44 +0100

    next.js RequestParams

--- app/test/[productId]/page.tsx
+++ app/test/[productId]/page.tsx
@@ -1,9 +1,12 @@
-export default function productId (
+export default async function productId (
+
+    {params} : Promise <{ params : {productId: string} }>
 
 ) {
+    const myParam = (await params).productId
     return (
         <main>
-            <h1>product</h1>
+            <p>The RequestParam is {myParam}</p>
 
         </main>
     )
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,6 +1,9 @@
+import { error } from "console";
 import Link from "next/link";
 
 export default function Test() {
+
+    
     return (
         <main>
             <h1>This is the Test Page</h1>
